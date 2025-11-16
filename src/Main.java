5b5a4ff16a528262fa17864376c5889188478f05
Sarsenov <zhanbek.sarsenov@gmail.com>
2025-11-16 20:52:21 +0500

    Реалізує завдання

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,39 @@
+import model.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Teacher> teachers = new ArrayList<>();
+
+        Teacher teacherVoinova = new Teacher("Voinova");
+        Teacher teacherKem = new Teacher("Kem");
+        Teacher teacherSemenyanya = new Teacher("Semenyanya");
+        Teacher teacherTkachuk = new Teacher("Tkachuk");
+        Teacher teacherBalashov = new Teacher("Balashov");
+        Teacher teacherBarinov = new Teacher("Barinov");
+
+        teachers.add(teacherVoinova);
+        teachers.add(teacherKem);
+        teachers.add(teacherSemenyanya);
+        teachers.add(teacherTkachuk);
+        teachers.add(teacherBalashov);
+        teachers.add(teacherBarinov);
+
+        System.out.println();
+        System.out.println("Список моїх вчителів:");
+        System.out.println("---------------------------");
+        for (int i = 0; i<teachers.size(); i++) {
+            System.out.println(i + " : " + teachers.get(i).getName());
+        }
+        System.out.println("---------------------------");
+
+        int theBestTeacherIndex = teachers.indexOf(teacherSemenyanya);
+        int theWorstTeacherIndex = teachers.indexOf(teacherBarinov);
+
+        System.out.println();
+        System.out.println("Індекс найкращого вчителя '" + teacherSemenyanya.getName() + "' дорівнює: " + theBestTeacherIndex);
+        System.out.println("Індекс найгіршого вчителя '" + teacherBarinov.getName() + "' дорівнює: " + theWorstTeacherIndex);
     }
 }
